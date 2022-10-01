86c042be1e1afc78d34bc6e24f87432ca2f77d85
taylorvercillo <taylornvcasey@gmail.com>
2022-10-01 14:19:14 -0600

    index and show routes for members

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,2 +1,35 @@
 class MembersController < ApplicationController
+
+    rescue_from ActiveRecord::RecordInvalid, with: :record_invalid
+    
+    def index
+        members = Member.all 
+        render json: members 
+    end 
+
+    def show
+        member = Member.find_by(id: parmas[:id])
+        render json: member
+    end 
+
+    def create
+        member = Member.create(member_params)
+        render json: member, status: :successful
+    end 
+
+    def update 
+    end 
+
+    def destroy
+    end 
+
+    private 
+
+    def member_params
+        parmas.permit(:name, :picture)
+    end 
+
+    def record_invalid(invalid)
+        render json: { errors: "Validation Errors" }, status: :unprocessable_entity
+    end
 end
