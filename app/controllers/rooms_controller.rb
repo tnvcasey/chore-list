cb7e90211916b8a62f205c5bc40007234608bde8
taylorvercillo <taylornvcasey@gmail.com>
2022-10-01 14:28:37 -0600

    adding crud to room

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,2 +1,24 @@
 class RoomsController < ApplicationController
+
+    def index
+        rooms = Room.all 
+        render json: rooms
+    end 
+
+    def show
+        room = Room.find_by(id: params[:id])
+        render json: room
+    end 
+
+    def create
+        room = Room.create(room_params)
+        render json: room, status: :created
+    end 
+
+    private 
+
+    def room_params
+        params.permit(:location)
+    end
+
 end
